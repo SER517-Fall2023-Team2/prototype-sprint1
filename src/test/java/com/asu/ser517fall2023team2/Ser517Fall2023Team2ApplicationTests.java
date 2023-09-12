d579c48daae7bb58d752e0e636c1793102b1b8e2
Jacob Jose <jjose14@asu.edu>
2023-09-11 21:49:58 -0700

    Task-36 Converted to Java Spring project, added Docker, and updated template.

---
+++ src/test/java/com/asu/ser517fall2023team2/Ser517Fall2023Team2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asu.ser517fall2023team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ser517Fall2023Team2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
